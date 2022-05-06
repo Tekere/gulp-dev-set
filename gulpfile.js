0778e9f12fe8515e565fd89ae4dfa82877eaad63
SumimotoTakara <96.sumimoto@gmail.com>
2022-05-06 18:22:58 +0900

    Sassのバグを修正 TSを標準で無効化

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,28 @@
 'use strict'
 
 const gulp = require('gulp')
-const sass = require('gulp-sass')
+const sass =  require('gulp-sass')(require('sass'))
 const plumber = require('gulp-plumber')
 const notify = require('gulp-notify')
 const sourcemaps = require('gulp-sourcemaps')
 const autoprefixer = require('gulp-autoprefixer')
 const postcss = require('gulp-postcss')
 const mqpacker = require('css-mqpacker')
-const typescript = require('gulp-typescript')
+// const typescript = require('gulp-typescript')
 const babel = require('gulp-babel')
+var rename = require("gulp-rename");
+var ejs = require("gulp-ejs");
 const browserSync = require('browser-sync').create()
 
 //====================
 //  タスクの追加
 //====================
-gulp.task('ts', function () {
-  return gulp
-    .src('./src/ts/*.ts')
-    .pipe(typescript())
-    .pipe(gulp.dest('./src/js'))
-})
+// gulp.task('ts', function () {
+//   return gulp
+//     .src('./src/ts/*.ts')
+//     .pipe(typescript())
+//     .pipe(gulp.dest('./src/js'))
+// })
 
 gulp.task('sass', function () {
   return gulp
@@ -79,7 +81,7 @@ gulp.task('watch', () => {
 
   gulp.watch('./src/ejs/*.ejs', gulp.series('ejs'))
   gulp.watch('./src/scss/*.scss', gulp.series('sass'))
-  gulp.watch('./src/ts/*.ts', gulp.series('ts'))
+  // gulp.watch('./src/ts/*.ts', gulp.series('ts'))
   gulp.watch('./src/js/*.js', gulp.series('babel'))
   gulp.watch('./dist/**/*', browserReload)
 })
@@ -89,5 +91,5 @@ gulp.task('watch', () => {
 //====================
 gulp.task(
   'default',
-  gulp.series('sass', 'ejs', 'ts', 'babel', 'serve', 'watch')
+  gulp.series('sass', 'ejs', 'babel', 'serve', 'watch')
 )
